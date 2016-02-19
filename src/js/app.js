d46d24f7059fc7dfaf5034260e47ab387fad0ca7
Vsevolod Drobyshevskiy <vsevolod_drobyshevskiy@epam.com>
2016-02-19 15:40:24 +0300

    table render

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,55 @@
 (function() {
-    angular.module('RenderTreeApp', [])
-        .directive('renderTree', function() {
+    var app = angular.module('RenderTreeApp', []);
+
+    app.directive('renderTree', function() {
             return {
                 restrict: 'A',
                 scope: {
                     data: '='
                 },
                 templateUrl: './templates/tree.html',
-                controller: function ($scope) {
-                }
             }
-        })
-        .directive('tabView', function() {
+        });
+
+    app.directive('employees', function() {
             return {
-                restrict: 'A',
-                template: '<span>table here</span>'
+                restrict: 'E',
+                replace: true,
+                scope: {
+                    data: '='
+                },
+                templateUrl: './templates/employees.html',
+                controller: function($scope){
+                    console.dir($scope);
+                },
             }
-        })
-        .controller('tree-controller', function ($http, $scope) {
+        });
+
+    app.controller('tree-controller', function ($http, $scope) {
+
+            function parseData (input) {
+                if (input.unitOwner) {
+                    $scope.table.push(input.unitOwner)
+                };
+                if (input.childUnits) {
+                    for (var i=input.childUnits.length-1; i>=0; i--) {
+                        parseData(input.childUnits[i]);
+                    };
+                };
+            };
+
+            $scope.table = [];
             $http.get('./test-data/OrgChartData.json').success(function(data){
                 $scope.data = data[0];
+                parseData($scope.data);
             });
+
             $scope.collapse = function($event) {
-                for (var i=0; i < $event.target.classList.length; i++) {
+                for (var i=0; i < $event.path.length; i++) {
                     if ($event.path[i].tagName == 'LI') {
                         $event.path[i].classList.toggle('collapsed');
                         break;
-                    }
+                    };
                 };
             };
 
